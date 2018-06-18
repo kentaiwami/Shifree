4d11fcc15a1e9340d5843df9f32bf78b655826b5
Kenta Iwami <g2117009@fun.ac.jp>
2018-06-19 07:50:54 +0900

    結合セルをに対応するための実装をした

--- API/analyze/company1.py
+++ API/analyze/company1.py
@@ -5,6 +5,7 @@ import mojimoji
 import xml.etree.ElementTree as ET
 from datetime import datetime as DT
 import datetime
+import inspect
 
 
 tmp_file_path = ''
@@ -21,7 +22,8 @@ def create_main(company_id, title, number, start, end, file):
         results = subprocess.check_output(['pdf2txt.py', tmp_file_path, '-t', 'xml'])
     except ValueError:
         os.remove(tmp_file_path)
-        raise Exception('解析コマンドの実行中にエラーが発生しました')
+        frame = inspect.currentframe()
+        raise Exception('解析コマンドの実行中にエラーが発生しました[{}]'.format(frame.f_lineno))
 
     page = ET.fromstring(results)[0]
 
@@ -30,11 +32,8 @@ def create_main(company_id, title, number, start, end, file):
     day_x_list = get_day_x(start, same_line_list, day_line_index)
     users_line = get_user_line(company_id, same_line_list, day_x_list[0]['x'], number)
     should_join_shift = get_should_join_shift(users_line)
-
-    # TODO 日付ごとにシフトをまとめる（その際に、結合も考慮）
-    users_shift_list = get_user_shift(users_line, day_x_list, should_join_shift)
-
-    # TODO 空文字を連結する処理
+    users_shift_list = get_user_shift(users_line, day_x_list)
+    joined_users_shift = get_joined_users_shift(users_shift_list, should_join_shift)
 
 
 
@@ -75,7 +74,8 @@ def get_x_y_text_from_xml(page):
 
     if len(x_y_text_list) == 0:
         os.remove(tmp_file_path)
-        raise Exception('情報抽出中にエラーが発生しました')
+        frame = inspect.currentframe()
+        raise Exception('情報抽出中にエラーが発生しました[{}]'.format(frame.f_lineno))
 
     return x_y_text_list
 
@@ -117,7 +117,8 @@ def get_same_line_list(x_y_text_list):
 
     if day_line_index == -1:
         os.remove(tmp_file_path)
-        raise Exception('日付の判定中にエラーが発生しました')
+        frame = inspect.currentframe()
+        raise Exception('日付の判定中にエラーが発生しました[{}]'.format(frame.f_lineno))
 
     return x_sorted_same_line_list, day_line_index
 
@@ -141,7 +142,8 @@ def get_day_x(start, same_line_list, day_line_index):
 
         if len(tmp_current_date) >= 3:
             os.remove(tmp_file_path)
-            raise Exception('日付の解析中にエラーが発生しました')
+            frame = inspect.currentframe()
+            raise Exception('日付の解析中にエラーが発生しました[{}]'.format(frame.f_lineno))
 
         if current_date == tmp_current_date:
             day_x_list.append({'day': current_date, 'x': date_x_y_text['x']})
@@ -189,7 +191,8 @@ def get_user_line(company_id, same_line_list, first_day_limit, number):
 
     if number != len(users_line):
         os.remove(tmp_file_path)
-        raise Exception('シフト表に記載されているユーザの人数が一致しません')
+        frame = inspect.currentframe()
+        raise Exception('シフト表に記載されているユーザの人数が一致しません[{}]'.format(frame.f_lineno))
 
     return users_line
 
@@ -225,68 +228,156 @@ def get_should_join_shift(users_line):
 
         while search_index < len(candidate_join_shift) - 1:
             if candidate_join_shift[search_index] != candidate_join_shift[search_index+1]:
-                join_start_end.append({'s': candidate_join_shift[start], 'e': candidate_join_shift[search_index]})
+                join_start_end.append({'start': candidate_join_shift[start], 'end': candidate_join_shift[search_index]})
                 start = search_index + 1
 
             search_index += 2
 
-        join_start_end.append({'s': candidate_join_shift[start], 'e': candidate_join_shift[search_index]})
+        join_start_end.append({'start': candidate_join_shift[start], 'end': candidate_join_shift[search_index]})
         results.append(join_start_end)
 
     return results
 
 
-def get_user_shift(users_line, day_x_list, should_join_shift):
+def get_user_shift(users_line, day_x_list):
     """
     全ユーザのシフトを日付ごとにまとめる。結合セルがあった場合は空文字として登録する。
-    :param users_line:          全ユーザのシフト情報が記載された2次元配列
-    :param day_x_list:          日付の場所が記載された1次元配列
-    :param should_join_shift:   aa
+    :param users_line:          全ユーザのシフト情報が格納された2次元配列
+    :param day_x_list:          日付の場所が格納された1次元配列
     :return:                    全ユーザ×全日付の2次元配列
     """
 
-    threshold_x = 10.0
+    threshold_x = 9.0
     results = []
 
     for user in users_line:
-        shift_list = users_line[4]['line'][users_line[4]['shift_start']:]
-        # shift_list = user['line'][user['shift_start']:]
+        shift_list = user['line'][user['shift_start']:]
         usr_result = []
         day_index = 0
         shift_index = 0
-        tmp_current_day_shift = ''
-        tmp_shift_x = 0.0
-        tmp_shift_x_count = 0.0
+        tmp_current_day_shift = []
 
         while len(day_x_list)-1 >= day_index and len(shift_list)-1 >= shift_index:
-            print(shift_list[shift_index]['text'], day_x_list[day_index]['day'], tmp_current_day_shift)
             if abs(shift_list[shift_index]['x'] - day_x_list[day_index]['x']) <= threshold_x:
-                tmp_current_day_shift += shift_list[shift_index]['text']
-                tmp_shift_x += shift_list[shift_index]['x']
-                tmp_shift_x_count += 1
+                tmp_current_day_shift.append(shift_list[shift_index])
                 shift_index += 1
             else:
                 usr_result.append({
-                    'shift': tmp_current_day_shift,
-                    'x': tmp_shift_x / tmp_shift_x_count
+                    'day': day_x_list[day_index]['day'],
+                    'shift': tmp_current_day_shift
                 })
-                tmp_current_day_shift = ''
-                tmp_shift_x = 0.0
-                tmp_shift_x_count = 0.0
+                tmp_current_day_shift = []
                 day_index += 1
 
         results.append(usr_result)
 
-        break
-
     # 全ユーザのシフトが日数分だけ存在するかチェック
     if len(list(filter(lambda x: len(x) != len(day_x_list), results))) != 0:
-        raise Exception('シフトの抽出結果に誤りがあったためエラーが発生しました')
+        frame = inspect.currentframe()
+        raise Exception('シフトの抽出結果に誤りがあったためエラーが発生しました[{}]'.format(frame.f_lineno))
 
-    for hoge in results:
-        print(hoge)
-        print('\n')
     return results
+
+
+def get_joined_users_shift(users_shift, should_join_shift):
+    """
+    ユーザのシフトの中で、結合すべきシフトや空文字となっているシフトを結合してエクセル上の結合セルと整合性を取る
+    :param users_shift:         ユーザごと×全日付の2次元配列
+    :param should_join_shift:   ユーザごとの結合すべきシフトが格納された2次元配列
+    :return:                    ユーザごとの結合済みのシフトが格納された2次元配列
+    """
+
+    results = []
+
+    for user_shift, join_shift in zip(users_shift, should_join_shift):
+        tmp_results = []
+        skip = -1
+
+        for i, day in enumerate(user_shift):
+            # シフト文字が結合されている分だけループをスキップ
+            if skip > 0:
+                skip -= 1
+                continue
+            else:
+                skip = -1
+
+            # shouldのstartに含まれているかどうか
+            shift_names = get_search_results_shift_name(day, join_shift, user_shift[i + 1:])
+
+            if shift_names is None:
+                # shift内のリスト内のテキストを結合してその日のシフト名とする
+                shift_name = [x['text'] for x in day['shift']]
+                tmp_results.append(''.join(shift_name))
+            else:
+                skip = len(shift_names) - 1
+                for shift_name in shift_names:
+                    tmp_results.append(shift_name)
+
+        results.append(tmp_results)
+
+    return results
+
+
+def get_search_results_shift_name(current_day_shift, join_shift, after_current_day_shifts):
+    """
+    結合開始・終了に含まれているかを検索。
+    含まれている場合は、終了地点までのシフト文字列を結合した結果を返す
+    含まれていない場合は、単純に結合した文字列を返す
+    :param current_day_shift:           対象となっている1日分のシフト情報
+    :param join_shift:                  結合開始・終了位置と対象ディクショナリを格納した1次元配列
+    :param after_current_day_shifts:    current_day_shift以降の1次元配列
+    :return:                            シフトの文字列が格納された1次元配列
+    """
+
+    if len(current_day_shift['shift']) == 0:
+        return ' '
+
+    found_shift = list(filter(lambda x: x['start'] == current_day_shift['shift'][0], join_shift))
+
+    if len(found_shift) == 0:
+        return None
+
+    found_shift = found_shift[0]
+    found_start_shift_index = current_day_shift['shift'].index(found_shift['start'])
+
+    # 連結開始のシフト文字が0番目以外で見つかる場合は、そもそもシフトを日付で分断する際に失敗している
+    if found_start_shift_index != 0:
+        frame = inspect.currentframe()
+        raise Exception('シフトの解析エラーが発生しました[{}]'.format(frame.f_lineno))
+
+    found_end_shift = list(filter(lambda x: x == found_shift['end'], current_day_shift['shift']))
+
+    # startとendが1日にあるため単純に結合して返す
+    if len(found_end_shift) != 0:
+        shift_names = [x['text'] for x in current_day_shift['shift']]
+        return [''.join(shift_names)]
+
+    # startとendが違う場所にある場合に探す
+    is_found = False
+    for i, after_current_day_shift in enumerate(after_current_day_shifts):
+        for shift in after_current_day_shift['shift']:
+            if shift == found_shift['end']:
+                is_found = True
+                break
+
+        if is_found:
+            break
+
+    # 結合して日数分だけ同じのを返す
+    join_range_shift = after_current_day_shifts[:i+1]
+    join_range_shift.insert(0, current_day_shift)
+    shift_name = ''
+
+    for day in join_range_shift:
+        hoge = [x['text'] for x in day['shift']]
+        shift_name += ''.join(hoge)
+
+    results = []
+    for i in range(0, len(join_range_shift)):
+        results.append(shift_name)
+
+    return results
+
 
 
 def update_main(table_id):
