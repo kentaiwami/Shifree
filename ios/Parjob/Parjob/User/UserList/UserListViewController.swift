97152f49db0107f3b1b76e69659beccb1dafc454
Kenta Iwami <g2117009@fun.ac.jp>
2018-07-04 21:15:12 +0900

    テキストエリアの設置

--- ios/Parjob/Parjob/User/UserList/UserListViewController.swift
+++ ios/Parjob/Parjob/User/UserList/UserListViewController.swift
@@ -29,53 +29,17 @@ class UserListViewController: FormViewController, UserListViewInterface {
     fileprivate func initializeForm() {
         UIView.setAnimationsEnabled(false)
         
-        var count = 0
-        let msg = "・既に登録しているユーザを削除して同じ名前のユーザを登録しても、新規登録となるので注意してください。\n・シフト表に同姓同名のユーザが記載されている場合は、並び順をシフト表の記載順と一致させる必要があります。"
+        let section = Section()
         
-        form +++
-            MultivaluedSection(
-            multivaluedOptions: [.Reorder, .Insert, .Delete],
-            header: "",
-            footer: msg) {
-                $0.tag = "textfields"
-                $0.addButtonProvider = { section in
-                    return ButtonRow(){
-                        $0.title = "ユーザを追加"
-                    }.cellUpdate({ (cell, row) in
-                        cell.textLabel?.textAlignment = .left
-                    })
-                }
-                
-                let defaultTitle = "タップしてユーザ情報を入力"
-                
-                $0.multivaluedRowToInsertAt = { index in
-                    return ButtonRow() {
-                        $0.title = defaultTitle
-                        $0.value = defaultTitle
-                        $0.tag = String(count) + "_new"
-                        count += 1
-                    }.cellUpdate({ (cell, row) in
-                        cell.textLabel?.textAlignment = .left
-                        if row.title! == defaultTitle {
-                            cell.textLabel?.textColor = .gray
-                        }else {
-                            cell.textLabel?.textColor = .black
-                        }
-                    })
-                }
-                
-                for user in presenter.getUserList() {
-                    $0 <<< ButtonRow() {
-                        $0.title = String(format: "%@ (%@)", arguments: [user.name, user.role])
-                        $0.value = String(format: "%@,%@,%@", arguments: [user.name, user.role, user.code])
-                        $0.cell.textLabel?.numberOfLines = 0
-                        $0.tag = String(user.code) + "_exist"
-                        }.cellUpdate({ (cell, row) in
-                            cell.textLabel?.textAlignment = .left
-                            cell.textLabel?.textColor = .black
-                        })
-                }
-        }
+            for user in presenter.getUserList() {
+                let textArea = TextAreaRow()
+                textArea.value =  String(format: "ユーザ名：%@\nコード：%@\n権限：%@\nパスワード：%@", arguments: [user.name, user.code, user.role, user.password])
+                textArea.disabled = true
+                textArea.cell.textLabel?.numberOfLines = 0
+                section.append(textArea)
+            }
+        
+        form.append(section)
         
         UIView.setAnimationsEnabled(true)
     }
