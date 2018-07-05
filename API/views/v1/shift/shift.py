f3c8ebf90492144bf50dfd5eb4009ef22d107764
Kenta Iwami <g2117009@fun.ac.jp>
2018-07-06 01:58:43 +0900

    シフトカテゴリのidも一緒に返すようにした

--- API/views/v1/shift/shift.py
+++ API/views/v1/shift/shift.py
@@ -14,11 +14,11 @@ def get():
     shift_category_list = session.query(Shift, ShiftCategory).join(ShiftCategory, Company).filter(ShiftCategory.company_id == user.company_id).order_by(ShiftCategory.id.asc()).all()
 
     results = []
-    current_category = ''
+    current_category = shift_category_list[0][1]
     tmp_shifts = []
 
     for shift_category in shift_category_list:
-        if current_category == shift_category[1].name:
+        if current_category == shift_category[1]:
             tmp_shifts.append({
                 'id': shift_category[0].id,
                 'name': shift_category[0].name,
@@ -26,7 +26,7 @@ def get():
                 'end': shift_category[0].end.strftime("%H:%M") if shift_category[0].end != None else None
             })
         else:
-            results.append({'category': current_category, 'shifts': tmp_shifts})
+            results.append({'category_name': current_category.name, 'category_id': current_category.id, 'shifts': tmp_shifts})
             tmp_shifts = []
             tmp_shifts.append({
                 'id': shift_category[0].id,
@@ -34,10 +34,7 @@ def get():
                 'start': shift_category[0].start.strftime("%H:%M") if shift_category[0].start != None else None,
                 'end': shift_category[0].end.strftime("%H:%M") if shift_category[0].end != None else None
             })
-            current_category = shift_category[1].name
-
-    # ループを単純にするために、最初に空の情報が入ってしまうので選別
-    results = [result for result in results if result['category'] != '']
+            current_category = shift_category[1]
 
     session.close()
     return jsonify({'results': results}), 200
