9b94271317c99596a7b02081aab5402d49533532
Kenta Iwami <g2117009@fun.ac.jp>
2018-07-06 01:37:35 +0900

    シフト情報を取得するAPIを実装

--- API/views/v1/shift/shift.py
+++ API/views/v1/shift/shift.py
@@ -11,15 +11,33 @@ app = Blueprint('shift_bp', __name__)
 @api_basic_auth.login_required
 def get():
     user = session.query(User).filter(User.code == api_basic_auth.username()).one()
+    shift_category_list = session.query(Shift, ShiftCategory).join(ShiftCategory, Company).filter(ShiftCategory.company_id == user.company_id).order_by(ShiftCategory.id.asc()).all()
 
-    shifts = session.query(Shift).join(ShiftCategory, Company).filter(ShiftCategory.company_id == user.company_id).all()
     results = []
+    current_category = ''
+    tmp_shifts = []
 
-    for shift in shifts:
-        results.append({
-            'id': shift.id,
-            'name': shift.name
-        })
+    for shift_category in shift_category_list:
+        if current_category == shift_category[1].name:
+            tmp_shifts.append({
+                'id': shift_category[0].id,
+                'name': shift_category[0].name,
+                'start': shift_category[0].start.strftime("%H:%M") if shift_category[0].start != None else None,
+                'end': shift_category[0].end.strftime("%H:%M") if shift_category[0].end != None else None
+            })
+        else:
+            results.append({'category': current_category, 'shifts': tmp_shifts})
+            tmp_shifts = []
+            tmp_shifts.append({
+                'id': shift_category[0].id,
+                'name': shift_category[0].name,
+                'start': shift_category[0].start.strftime("%H:%M") if shift_category[0].start != None else None,
+                'end': shift_category[0].end.strftime("%H:%M") if shift_category[0].end != None else None
+            })
+            current_category = shift_category[1].name
+
+    # ループを単純にするために、最初に空の情報が入ってしまうので選別
+    results = [result for result in results if result['category'] != '']
 
     session.close()
     return jsonify({'results': results}), 200
