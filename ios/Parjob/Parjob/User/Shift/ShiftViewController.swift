dd5a702498e8733585f68ad49dd8aab9280b958e
Kenta Iwami <g2117009@fun.ac.jp>
2018-07-06 01:52:44 +0900

    ファイルをコピーして、user設定画面からの遷移を記述

---
+++ ios/Parjob/Parjob/User/Shift/ShiftViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ShiftViewController.swift
+//  Parjob
+//
+//  Created by 岩見建汰 on 2018/07/01.
+//  Copyright © 2018年 Kenta Iwami. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+protocol ShiftViewInterface: class {
+    var formValues: [String:Any?] { get }
+    
+    func initializeUI()
+    func success()
+    func showErrorAlert(title: String, msg: String)
+}
+
+
+class ShiftViewController: FormViewController, ShiftViewInterface {
+    
+    fileprivate var presenter: ShiftViewPresenter!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presenter = ShiftViewPresenter(view: self)
+        presenter.setShiftCategory()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationItem.title = "Shift Setting"
+    }
+    
+    fileprivate func initializeForm() {
+        UIView.setAnimationsEnabled(false)
+        
+        var count = 0
+        
+        form +++ MultivaluedSection(
+            multivaluedOptions: [.Insert, .Delete],
+            header: "",
+            footer: "既にあるカテゴリを削除して同じ名前のカテゴリを登録しても、新規登録となるので注意してください。") {
+                $0.addButtonProvider = { section in
+                    section.showInsertIconInAddButton = true
+                    return ButtonRow(){
+                        $0.title = "カテゴリを追加"
+                        }.cellUpdate({ (cell, row) in
+                            cell.textLabel?.textAlignment = .left
+                        })
+                }
+
+                $0.multivaluedRowToInsertAt = { _ in
+                    return NameRow() {
+                        $0.placeholder = "カテゴリ名"
+                        $0.tag = String(count) + "_new"
+                        count += 1
+                    }
+                }
+
+                for shiftCategory in presenter.getShiftCategory() {
+                    $0 <<< NameRow() {
+                        $0.value = shiftCategory.name
+                        $0.tag = String(shiftCategory.id) + "_exist"
+                    }
+                }
+        }
+        
+        presenter.setInitShiftCategory(values: form.values())
+
+        UIView.setAnimationsEnabled(true)
+    }
+    
+    fileprivate func initializeNavigationItem() {
+        let check = UIBarButtonItem(image: UIImage(named: "checkmark"), style: .plain, target: self, action: #selector(tapEditDoneButton))
+        self.navigationItem.setRightBarButton(check, animated: true)
+    }
+    
+    @objc private func tapEditDoneButton() {
+        presenter.updateShiftCategory()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
+
+// MARK: - formでユーザが設定する値
+extension ShiftViewController {
+    var formValues: [String:Any?] {
+        return self.form.values()
+    }
+}
+
+// MARK: - Presenterから呼び出される関数
+extension ShiftViewController {
+    func initializeUI() {
+        initializeNavigationItem()
+        initializeForm()
+    }
+    
+    func success() {
+        ShowStandardAlert(title: "Success", msg: "情報を更新しました", vc: self) {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func showErrorAlert(title: String, msg: String) {
+        ShowStandardAlert(title: title, msg: msg, vc: self, completion: nil)
+    }
+}
---
+++ ios/Parjob/Parjob/User/Shift/ShiftViewPresenter.swift
@@ -0,0 +1,52 @@
+//
+//  ShiftViewPresenter.swift
+//  Parjob
+//
+//  Created by 岩見建汰 on 2018/07/01.
+//  Copyright © 2018年 Kenta Iwami. All rights reserved.
+//
+
+import Foundation
+
+class ShiftViewPresenter {
+    
+    weak var view: ShiftViewInterface?
+    let model: ShiftViewModel
+    
+    init(view: ShiftViewInterface) {
+        self.view = view
+        self.model = ShiftViewModel()
+        model.delegate = self
+    }
+    
+    func setShiftCategory() {
+        model.setShiftCategory()
+    }
+    
+    func getShiftCategory() -> [ShiftCategory] {
+        return model.shiftCategory
+    }
+    
+    func updateShiftCategory() {
+        guard let values = view?.formValues else {return}
+        model.updateShiftCategory(values: values)
+    }
+    
+    func setInitShiftCategory(values: [String:Any?]) {
+        model.setInitShiftCategory(values: values)
+    }
+}
+
+extension ShiftViewPresenter: ShiftViewModelDelegate {
+    func initializeUI() {
+        view?.initializeUI()
+    }
+    
+    func success() {
+        view?.success()
+    }
+    
+    func faildAPI(title: String, msg: String) {
+        view?.showErrorAlert(title: title, msg: msg)
+    }
+}
