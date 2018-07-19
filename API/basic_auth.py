4985637da047a4d2e9cc91cdd1f25390de476a6e
Kenta Iwami <g2117009@fun.ac.jp>
2018-07-19 22:16:39 +0900

    demoユーザでアクセスされた場合は、ユーザのログインに関わる情報更新と認証のスキップをするようにした

--- API/basic_auth.py
+++ API/basic_auth.py
@@ -2,6 +2,7 @@ from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import check_password_hash
 from database import session
 from model import User
+from config import demo_admin_user, demo_general_user
 
 api_basic_auth = HTTPBasicAuth()
 
@@ -14,4 +15,7 @@ def verify_pw(user_code, password):
     if user is None:
         return False
     else:
+        if user.code == demo_general_user['code'] or user.code == demo_admin_user['code']:
+            return True
+
         return check_password_hash(user.password, password)
--- API/views/v1/setting/username.py
+++ API/views/v1/setting/username.py
@@ -4,6 +4,7 @@ from jsonschema import validate, ValidationError
 from model import User
 from database import session
 from basic_auth import api_basic_auth
+from config import demo_general_user, demo_admin_user
 
 app = Blueprint('setting_username_bp', __name__)
 
@@ -25,6 +26,13 @@ def update():
         abort(400, {'code': frame.f_lineno, 'msg': e.message, 'param': None})
 
     user = session.query(User).filter(User.code == api_basic_auth.username()).one()
+
+
+    if user.code == demo_admin_user['code'] or user.code == demo_general_user['code']:
+        session.close()
+        return jsonify({'results': {'name': user.name}}), 200
+
+
     user.name = request.json['username']
 
     session.commit()
