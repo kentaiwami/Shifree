3fac496c5d4493700aa0dc1697f0f7ae3fe31a4c
Kenta Iwami <g2117009@fun.ac.jp>
2018-09-21 14:11:00 +0900

    給与画面のメッセージを修正

--- ios/Parjob/Parjob/User/Salary/SalaryViewController.swift
+++ ios/Parjob/Parjob/User/Salary/SalaryViewController.swift
@@ -20,7 +20,7 @@ class SalaryViewController: UIViewController, ScrollableGraphViewDataSource, Sal
     
     fileprivate var presenter: SalaryViewPresenter!
     var graphView = ScrollableGraphView()
-    var emptyView = getEmptyView(msg: EmptyMessage.becauseNoImportShiftFile.rawValue)
+    var emptyView = getEmptyView(msg: EmptyMessage.becauseNotFoundShiftFIle.rawValue)
     
     override func viewDidLoad() {
         super.viewDidLoad()
