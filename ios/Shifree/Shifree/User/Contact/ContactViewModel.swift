c3743dd51b4df8ed18a775c815e3ff574f5db5dd
Kenta Iwami <g2117009@fun.ac.jp>
2018-09-28 13:49:50 +0900

    お問い合わせ用のmvpを用意

---
+++ ios/Shifree/Shifree/User/Contact/ContactViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ContactViewModel.swift
+//  Shifree
+//
+//  Created by 岩見建汰 on 2018/07/01.
+//  Copyright © 2018年 Kenta Iwami. All rights reserved.
+//
+
+import Foundation
+import KeychainAccess
+
+protocol ContactViewModelDelegate: class {
+    func success()
+    func faildAPI(title: String, msg: String)
+}
+
+class ContactViewModel {
+    weak var delegate: ContactViewModelDelegate?
+    private let api = API()
+    
+    func getUsername() -> String {
+        let keychain = Keychain()
+        return (try! keychain.get("userName"))!
+    }
+    
+    func updateUserName(newUserName: String) {
+        api.updateUserName(newUserName: newUserName).done { (json) in
+            let keychain = Keychain()
+            try! keychain.set(json["name"].stringValue, key: "userName")
+            self.delegate?.success()
+        }
+        .catch { (err) in
+            let tmp_err = err as NSError
+            let title = "Error(" + String(tmp_err.code) + ")"
+            self.delegate?.faildAPI(title: title, msg: tmp_err.domain)
+        }
+    }
+}
---
+++ ios/Shifree/Shifree/User/Contact/ContactViewPresenter.swift
@@ -0,0 +1,45 @@
+//
+//  ContactViewPresenter.swift
+//  Shifree
+//
+//  Created by 岩見建汰 on 2018/07/01.
+//  Copyright © 2018年 Kenta Iwami. All rights reserved.
+//
+
+import Foundation
+
+protocol ContactViewPresentable: class {
+    var username: String { get }
+}
+
+class ContactViewPresenter {
+    
+    weak var view: ContactViewInterface?
+    let model: ContactViewModel
+    
+    init(view: ContactViewInterface) {
+        self.view = view
+        self.model = ContactViewModel()
+        model.delegate = self
+    }
+    
+    var username: String {
+        return model.getUsername()
+    }
+    
+    func updateUserName() {
+        guard let username = view?.username else {return }
+        model.updateUserName(newUserName: username)
+    }
+    
+}
+
+extension ContactViewPresenter: ContactViewModelDelegate {
+    func success() {
+        view?.success()
+    }
+    
+    func faildAPI(title: String, msg: String) {
+        view?.showErrorAlert(title: title, msg: msg)
+    }
+}
