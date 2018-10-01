98db39ae1691b5d11e1b3f8cc7283f6bd5e2683d
Kenta Iwami <g2117009@fun.ac.jp>
2018-10-02 02:35:06 +0900

    url修正

--- ios/Shifree/Shifree/User/PrivacyPolicy/PrivacyPolicyViewModel.swift
+++ ios/Shifree/Shifree/User/PrivacyPolicy/PrivacyPolicyViewModel.swift
@@ -12,5 +12,5 @@ protocol PrivacyPolicyViewModelDelegate: class {}
 
 class PrivacyPolicyViewModel {
     weak var delegate: PrivacyPolicyViewModelDelegate?
-    let urlRequest = URLRequest(url: URL(string: GetPortfolioHost() + "portfolio/7/pp")!)
+    let urlRequest = URLRequest(url: URL(string: GetPortfolioHost() + "7/pp")!)
 }
