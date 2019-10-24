0f30ccb1c69df80f5995785f4d0d7b8e5e61a455
Kenta Iwami <kentaiwami12@gmail.com>
2019-10-24 18:17:29 +0900

    add init data script

---
+++ server/src/fixtures/init/init_data.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import glob
+import os
+os.chdir("./fixtures/init")
+
+config = {
+    'user': os.environ['MYSQL_USER'],
+    'password': os.environ['MYSQL_PASSWORD'],
+    'host': os.environ['MYSQL_HOST'],
+    'database' : os.environ['MYSQL_DATABASE'],
+}
+
+
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+
+def execute_scripts_from_file(filename):
+    fd = open(filename, 'r')
+    sqlFile = fd.read()
+    fd.close()
+    sqlCommands = sqlFile.split(';')
+
+    for command in sqlCommands:
+        try:
+            if command.strip() != '':
+                cursor.execute(command)
+        except IOError:
+            print('Command skipped: ' + command)
+
+def get_sql_files():
+    sql_files = glob.glob('./*.sql')
+    sql_files.sort()
+    return sql_files
+
+for sql_file in get_sql_files():
+    execute_scripts_from_file(sql_file)
+
+cnx.commit()
+cursor.close()
+cnx.close()
